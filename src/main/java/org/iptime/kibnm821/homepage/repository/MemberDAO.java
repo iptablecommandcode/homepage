d8fcb60b9e6e4d443b74befbfae0a3f2af8f3b65
iptablecommandcode <iptablecommandcode@gmail.com>
2021-03-10 16:02:20 +0900

    make branch

--- src/main/java/org/iptime/kibnm821/homepage/repository/MemberDAO.java
+++
@@ -1,7 +0,0 @@
-package org.iptime.kibnm821.homepage.repository;
-
-import java.util.Map;
-
-public interface MemberDAO {
-    public Object SelectAccount(Map<String,Object> dataMap);
-}
--- src/main/java/org/iptime/kibnm821/homepage/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package org.iptime.kibnm821.homepage.service;
-
-import java.util.Map;
-
-public interface MemberService {
-    public Object LoginCheck(Map<String,Object> dataMap);
-}
